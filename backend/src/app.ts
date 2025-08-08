965c630092ad66ddfcbab113564f21a85ff70d56
guruakaashjn <gurujnn@gmail.com>
2025-08-08 17:30:53 +0530

    add files for initial backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./config/express";
+
+const server = async () => {
+  try {
+    console.log("Starting to run the server");
+
+    // connect to mongo db configurations.
+
+    app.listen(process.env.SERVER_PORT, () => {
+      console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+    });
+  } catch (err) {
+    console.log("Server error", err);
+    process.exit();
+  }
+};
+
+server();
---
+++ backend/src/config/express.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { useErrorHandler } from "../middlewares/errorHandler";
+
+const app = express();
+app.use(cors({ exposedHeaders: ["X-Total-Items"] }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api/v1/user", userRoutes);
+// app.use("/api/v1/todo", todoRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use(useErrorHandler);
+
+export default app;
